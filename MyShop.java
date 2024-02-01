cca6ea425b9fbfe0691479c0d8945b1575a7657a
PrernaSapkal <141305557+PrernaSapkal@users.noreply.github.com>
2024-02-01 21:20:55 +0530

    Add files via upload

---
+++ MyShop.java
@@ -0,0 +1,14 @@
+package com.MyShop;
+
+public interface MyShop {
+	public String getProductId();
+	
+	public String getName();
+	
+	public String getPrice();
+	
+	public String getCateogory();
+	
+	
+
+}
